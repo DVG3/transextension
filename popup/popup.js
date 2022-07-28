b7c32b9c6efcb28c40408e0e4d5c68e014e58d7b
DVG3 <lapminh02022006@gmail.com>
2022-07-28 20:01:34 +0700

    change textbox

--- popup/popup.js
+++ popup/popup.js
@@ -5,7 +5,6 @@ document.getElementById("sendbtn").addEventListener("click",()=>{
 
 
 function sendMessage(text){
-
     fetch("https://discord.com/api/webhooks/1002076782964527236/z3P3Z__goca3NMkUhZaGPu3kjrHPxbhNjwVtY3UA0n4177zEwfE9HwT4ey4rqmi37Uk6",
     {
         method:"post",
